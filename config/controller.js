41d8cab745931bf4ad693b7f680f40f890d8b555
Vladimir Mironiuk <vladimironiuk@gmail.com>
2018-02-23 15:49:01 +0200

    Changed target dir for Windows

--- config/controller.js
+++ config/controller.js
@@ -1,9 +1,17 @@
 function Controller()
 {
     hideUnnecessaryPages();
+	
+    if (systemInfo.productType === "windows")
+        updateTargetDirForWindows();
 }
 
 function hideUnnecessaryPages()
 {
     installer.setDefaultPageVisible(QInstaller.StartMenuSelection, false);
+}
+
+function updateTargetDirForWindows()
+{
+    installer.setValue("TargetDir", "@ApplicationsDirX86@/Milo/qtcwizard/");
 }
